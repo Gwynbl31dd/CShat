9de52672e646dec7469a8836826d61e9cc35eb3f
Gwynbl31dd <paulin.anthony@gmail.com>
2017-04-08 14:59:30 +1000

    Add key pressed to send messages

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-var express = require('express'),
-	app = express(),
-	server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
-    ent = require('ent'), // Avoid HTML entities
-    path = require('path');
+/**
+ * @autor Paulin Anthony
+ * @since 06/04/2017
+ */
+const express = require('express'),
+	  app = express(),
+	  server = require('http').createServer(app),
+	  io = require('socket.io').listen(server),
+	  ent = require('ent'), // Avoid HTML entities
+	  path = require('path'),
+	  ejs = require('ejs');//Template engine
+
+var portNumber = 8080;
+
 //Load statics files
 app.use(express.static(path.join(__dirname, 'public')));
+
 // Load index.html
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/views/index.html');
+	res.render('index.ejs',{port:portNumber});
+});
+
+// Redirect to an error 404 if no page found
+app.use(function(req,res){
+	res.render('404.ejs');
 });
 
 io.sockets.on('connection', function (socket, login) {
-	
     // while we get a login, save and send it
     socket.on('new_client', function(login){
         login = ent.encode(login);
@@ -20,7 +33,6 @@ io.sockets.on('connection', function (socket, login) {
         socket.broadcast.emit('new_client', login);
         console.log('new user');
     });
-
     // When get a message, get the login and send it
     socket.on('message', function (message) {
         message = ent.encode(message);
@@ -29,4 +41,4 @@ io.sockets.on('connection', function (socket, login) {
     }); 
 });
 
-server.listen(8080);
+server.listen(portNumber);
